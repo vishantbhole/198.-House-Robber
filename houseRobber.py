6fc67da16144436cb8b6e8a76a1812d134d86f6c
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-07-31 21:57:54 -0500

    Update houseRobber.py

--- houseRobber.py
+++ houseRobber.py
@@ -6,6 +6,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+
+         # even = 0
+        # odd = 0
+        #
+        # for i, num in enumerate(nums):
+        #     if i % 2:
+        #         odd += num
+        #     else:
+        #         even += num
+        #
+        # return max(even,odd)
+
+        
         prev = 0
         prevnot = 0
 
